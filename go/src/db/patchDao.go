c8d3f7a515e7b1a516b9a974399fd89281dacb3b
amano <1060996790@qq.com>
2019-07-23 22:45:21 +0800

    add patch [ci skip]

--- go/src/db/patchDao.go
+++ go/src/db/patchDao.go
@@ -5,11 +5,22 @@ import (
 	"github.com/lin1heart/spider/go/src/util"
 )
 
-func QueryPhotoAfterRow(rowIndex int) map[int]map[string]string {
-	sqlString := fmt.Sprintf("SELECT * FROM photo LIMIT %d,50", rowIndex)
+func QueryPhotoAfterRow(rowIndex int, inteval int) map[int]map[string]string {
+	sqlString := fmt.Sprintf("SELECT * FROM photo WHERE (url != '' AND url is not null ) LIMIT %d,%d", rowIndex, inteval)
 	rows, err := Mysql.Query(sqlString)
 	util.CheckError(err)
 
 	results, err := ConvertToRows(rows)
 	return results
+}
+func UpdateUrlToEmpty(id int) {
+	stmt, err := Mysql.Prepare(`UPDATE photo SET url= "" WHERE id=?`)
+	util.CheckError(err)
+	defer stmt.Close()
+
+	res, err := stmt.Exec(id)
+	util.CheckError(err)
+	num, err := res.RowsAffected()
+	fmt.Printf("UpdatePhotoUrl id %d affect row %d \n", id, num)
+	util.CheckError(err)
 }
--- go/src/patch/validateUrl.go
+++ go/src/patch/validateUrl.go
@@ -1,23 +1,60 @@
 package patch
 
 import (
+	"crypto/tls"
 	"fmt"
 	"github.com/lin1heart/spider/go/src/db"
+	"github.com/lin1heart/spider/go/src/util"
+	"net/http"
+	"strconv"
 )
 
 func init() {
 	fmt.Println("patch init")
 }
 func main() {
-	startId := 0
-	//endId := 233648
-	endId := 150
+	startId := 118154
+	endId := 233648
+
+	inteval := 100
 
 	for index := startId; index < endId; {
 		fmt.Println("index ", index)
-		index += 50
-		results := db.QueryPhotoAfterRow(index)
 
-		fmt.Println("results", results)
+		results := db.QueryPhotoAfterRow(index, inteval)
+
+		for _, row := range results {
+			id, err := strconv.Atoi(row["id"])
+			util.CheckError(err)
+			valid := validate(row["url"], id)
+			if valid == false {
+				db.UpdateUrlToEmpty(id)
+			}
+		}
+		index += inteval
 	}
+}
+
+var tr = &http.Transport{
+	TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	//DisableKeepAlives: true,
+}
+var client = &http.Client{Transport: tr}
+
+func validate(url string, id int) bool {
+
+
+	resp, err := client.Get(url)
+	util.CheckError(err)
+
+	//defer func () {
+	//	err := resp.Body.Close()
+	//	util.CheckError(err)
+	//} ()
+	fmt.Printf("stauts code %d \n", resp.StatusCode)
+	if 404 == resp.StatusCode {
+		fmt.Printf("id %d 404 url %s \n", id, url)
+		return false
+	}
+	return true
 }
