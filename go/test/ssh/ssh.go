66734566e86019809fd1a8e15e10dc4d5b31d0bd
amanoooo <amanoooooooooooooooo@gmail.com>
2019-07-23 11:52:18 +0800

    add ssh example [ci skip]

---
+++ go/test/ssh/ssh.go
@@ -0,0 +1,114 @@
+package ssh
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/lin1heart/spider/go/src/util"
+	"golang.org/x/crypto/ssh"
+	"io/ioutil"
+	"net"
+	"time"
+)
+
+func connect(user, password, host, key string, port int, cipherList []string) (*ssh.Session, error) {
+	var (
+		auth         []ssh.AuthMethod
+		addr         string
+		clientConfig *ssh.ClientConfig
+		client       *ssh.Client
+		config       ssh.Config
+		session      *ssh.Session
+		err          error
+	)
+
+	fmt.Println("connect start")
+	// get auth method
+	auth = make([]ssh.AuthMethod, 0)
+	if key == "" {
+		auth = append(auth, ssh.Password(password))
+	} else {
+		pemBytes, err := ioutil.ReadFile(key)
+		if err != nil {
+			return nil, err
+		}
+
+		var signer ssh.Signer
+		if password == "" {
+			signer, err = ssh.ParsePrivateKey(pemBytes)
+		} else {
+			signer, err = ssh.ParsePrivateKeyWithPassphrase(pemBytes, []byte(password))
+		}
+		if err != nil {
+			return nil, err
+		}
+		auth = append(auth, ssh.PublicKeys(signer))
+	}
+
+	if len(cipherList) == 0 {
+		config = ssh.Config{
+			Ciphers: []string{"aes128-ctr", "aes192-ctr", "aes256-ctr", "aes128-gcm@openssh.com", "arcfour256", "arcfour128", "aes128-cbc", "3des-cbc", "aes192-cbc", "aes256-cbc"},
+		}
+	} else {
+		config = ssh.Config{
+			Ciphers: cipherList,
+		}
+	}
+
+	clientConfig = &ssh.ClientConfig{
+		User:    user,
+		Auth:    auth,
+		Timeout: 30 * time.Second,
+		Config:  config,
+		HostKeyCallback: func(hostname string, remote net.Addr, key ssh.PublicKey) error {
+			return nil
+		},
+	}
+
+	// connet to ssh
+	addr = fmt.Sprintf("%s:%d", host, port)
+
+	if client, err = ssh.Dial("tcp", addr, clientConfig); err != nil {
+		return nil, err
+	}
+
+	// create session
+	if session, err = client.NewSession(); err != nil {
+		return nil, err
+	}
+
+	modes := ssh.TerminalModes{
+		ssh.ECHO:          0,     // disable echoing
+		ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
+		ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
+	}
+
+	if err := session.RequestPty("xterm", 80, 40, modes); err != nil {
+		return nil, err
+	}
+
+	return session, nil
+}
+
+
+const (
+	username = "root"
+	password = ""
+	ip       = "39.104.226.149"
+	port     = 22
+	cmd      = "ls"
+	key = "/Users/hikaruamano/.ssh/id_rsa"
+
+)
+
+
+func main() {
+	ciphers := []string{}
+	session, err := connect(username, password, ip, key, port, ciphers)
+	util.CheckError(err)
+	defer session.Close()
+	var stdoutBuf bytes.Buffer
+	session.Stdout = &stdoutBuf
+	session.Run(cmd)
+
+	fmt.Println(session.Stdout)
+}
